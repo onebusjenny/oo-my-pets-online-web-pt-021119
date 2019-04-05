79351699279a780dbe5f1e09925d73111628712f
Jenny Chu <jchudesign0930@gmail.com>
2019-04-05 02:33:08 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,19 @@
 class Dog
   # code goes here
+  attr_accessor :mood
+  attr_reader :name
+  
+  def initialize(name)
+    @name = name
+    @mood = "nervous"
+  end
+  
+  def name=(name)
+    name.freez
+  end
+  
+  
+
+  
+  
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,28 @@
 class Owner
   # code goes here
+  attr_accessor :species, :name, :pets
+  
+  
+  
+  @@all = []
+  
+  def initialize(species)
+    @species = species
+    @name = name
+    @pets = {fishes: [], cats: [], dogs: []}
+  end
+  
+  def species= (species)
+    species.freez
+  end
+  
+  def say_species(species)
+    @species.each
+  end
+  
+  def buy_fish
+    self.fish  
+  end
+  
+  
 end
